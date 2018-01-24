6807c366e37e7f1e56703f5ac10034f3859540b0
Owcollier <owcollier@gmail.com>
2018-01-24 14:23:57 -0500

    continuing with push and pop

--- push-and-pop.js
+++ push-and-pop.js
@@ -22,8 +22,8 @@ main();
 // ===first push===
 // Array { length: 1, _capacity: 3, ptr: 0 }
 // 
-// ===second push===
+// ===after pushing 5 new values===
 // Array { length: 6, _capacity: 12, ptr: 3 }
 // Length has increased to 6 because we have pushed 6 items altogether to the array
-// Capacity has increased to 12 because when _resize was called on the 3rd push, it was passed a size of (this.length(3)+ 1) * Array.SIZE_RATIO(3))
-// ptr position also reflects where the resize took place on push # 3.
+// Capacity has increased to 12 because when _resize was called on the 4th push, it was passed a size of (this.length(3)+ 1) * Array.SIZE_RATIO(3))
+// ptr position reflects where the resize took place on push # 4 -> value of the head from push #1, where resize method is first called, now translates to ptr value after second resize.
